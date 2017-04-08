fcf942319955aaef3c1e8c202c35704be371fc31
Elibrighton <eli.brighton@gmail.com>
2017-04-09 09:20:10 +1000

    Add FileMover project

---
+++ FileMoverTests/FileMoverTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileMoverTests
+{
+    [TestClass]
+    public class FileMoverTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
